231191cbfbb05232c92c7b6f6f2e1b4776f6ddf9
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2026-06-05 19:54:35 +0530

    feat: implement CRUD API endpoints for categories with authentication support

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -80,3 +80,92 @@ export const POST = async (req: NextRequest) => {
     }, { status: 500 })
   }
 }
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const category = await prisma.category.findMany({
+      orderBy: { createdAt: "desc" }
+    })
+    return NextResponse.json({
+      success: true,
+      message: "category fatched successfully",
+      data: category
+    }, { status: 200 })
+
+  } catch (error) {
+    console.log("CATEGORY GET ERROR :", error)
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong in category service"
+    }, { status: 500 })
+  }
+}
+export const PATCH = async (req: NextRequest) => {
+  /*
+  take data form req.json
+  validate with the category schema
+  check user has token ,check jwt_secret is there or not, decode it 
+  check user is exist or not
+  check id is present or not
+  update the category if category not exist return error
+  return response
+  */
+  try {
+    const data = await req.json()
+    const validatedData = categorySchema.safeParse(data)
+    if (!validatedData.success) {
+      return NextResponse.json({
+        success: false,
+        message: "Invalid Input",
+        errors: validatedData.error.flatten().fieldErrors
+      }, { status: 400 })
+    }
+    const { name, slug } = validatedData.data
+    const token = req.cookies.get("auth_token")?.value
+    if (!token) {
+      return NextResponse.json({
+        success: false,
+        message: "Unauthorized : token not found"
+      }, { status: 401 })
+    }
+    if (!process.env.JWT_SECRET) {
+      return NextResponse.json({
+        success: false,
+        message: "JWT_SECRET not found "
+      }, { status: 500 })
+    }
+    const decode = jwt.verify(token, process.env.JWT_SECRET) as { id: string, email: string }
+
+    const findUser = await prisma.users.findUnique({
+      where: { id: decode.id }
+    })
+
+    if (!findUser) {
+      return NextResponse.json({
+        success: false,
+        message: "Unauthorized : user not found"
+      }, { status: 401 })
+    }
+
+    const updateCategory = await prisma.category.update({
+      where: { id: data.id },
+      data: {
+        name: name.toLowerCase(),
+        slug: slug.toLowerCase()
+      }
+    })
+
+    return NextResponse.json({
+      success: true,
+      message: "Category Updated Successfully",
+      data: updateCategory
+    }, { status: 200 })
+
+
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong in Category Update Service"
+    }, { status: 500 })
+  }
+}
